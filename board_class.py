01a1abd442f9f3bc0e3e51e23c581e9826d8471c
zsiga007 <zsiga007@users.noreply.github.com>
2020-02-26 19:43:31 +0100

    Update board_class.py

--- board_class.py
+++ board_class.py
@@ -1,17 +1,38 @@
-    class Board:
+RED = 0
+    BLACK = 1
+    NO_COLOUR = -1
+    RED_HOME = 0
+    BAR = -1
+    OUT = 24
+class Board:
+    #static members or methods
     RED = 0
     BLACK = 1
     NO_COLOUR = -1
     RED_HOME = 0
     BAR = -1
     OUT = 24
-
+##member
     def __init__(self, points_number, points_colour, bar, out):
         self.points_number = points_number
         self.points_colour = points_colour
         self.bar = bar
         self.out = out
-
+        ##fields
+        ##subclasses, default inheritance, overwriting
+def new_board():
+    return \
+        Board ([2, 0, 0, 0, 0, 5,
+               0, 3, 0, 0, 0, 5,
+               5, 0, 0, 0, 3, 0,
+               5, 0, 0, 0, 0, 2],
+                [BLACK, NO_COLOUR, NO_COLOUR, NO_COLOUR, NO_COLOUR, RED,
+                NO_COLOUR, RED, NO_COLOUR, NO_COLOUR, NO_COLOUR, BLACK,
+                RED, NO_COLOUR, NO_COLOUR, NO_COLOUR, BLACK, NO_COLOUR,
+                BLACK, NO_COLOUR, NO_COLOUR, NO_COLOUR, NO_COLOUR, RED],
+                [0, 0],
+                [0, 0])
+    @staticmethod 
     def opponent(self, player):
         return (player + 1) % 2
 
@@ -30,17 +51,22 @@
     def has_checkers_on_bar(board, player):
         return (board.bar[player] != 0)
 
-    def move(board, player, fromPoint, toPoint):  # assume that the labels of points run from 0 to 23
-        """
-        Assuming that the move is legitimate, actually carry it out.
-        The board gets modified!
-        """
-        if board.points_number[fromPoint] == 1:  ## I later realised we had a function for colour...
-            board.points_colour[fromPoint] = 'NO_COLOUR'
-        if board.points_number[toPoint] <= 1:
-            board.points_colour[toPoint] = player
-        board.points_number[fromPoint] = board.points_number[fromPoint] - 1
-        board.points_number[toPoint] = board.points_number[toPoint] + 1
-
-    
+    def move(board, player, fromPoint, toPoint):
+        if fromPoint==BAR:
+            board.bar[player]-=1
+        else:
+            if board.points_number[fromPoint] == 1:
+                board.points_colour[fromPoint] = 'NO_COLOUR'
+            if board.points_number[toPoint] <= 1:
+                board.points_colour[toPoint] = player
+            board.points_number[fromPoint] = board.points_number[fromPoint] - 1
+            if not board.points_colour[toPoint]==opponent(player):
+                board.points_number[toPoint] = board.points_number[toPoint] + 1
+            else:
+                board.bar[opponent(player)]+=1
+            if toPoint==OUT:
+                board.out[player]+=1
+class InitialBoard(Board):
+    def __init__(self):
+        self=new_board()
     
