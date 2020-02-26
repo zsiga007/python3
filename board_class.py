d782f5e9ac129b0eee56f9b817272df1acb43007
zsiga007 <zsiga007@users.noreply.github.com>
2020-02-26 15:17:45 +0100

    Update board_class.py

--- board_class.py
+++ board_class.py
@@ -1,18 +1,46 @@
-class Board:
-    def __init__(self,points_number,points_colour,bar,out):
-        self.points_number=points_number
-        self.points_colour=points_colour
-        self.bar=bar
-        self.out=out
+    class Board:
     RED = 0
     BLACK = 1
     NO_COLOUR = -1
     RED_HOME = 0
     BAR = -1
     OUT = 24
-    def opponent( player ):
-        return ( player + 1 ) % 2
-    def number_of_pieces( board, point ):
+
+    def __init__(self, points_number, points_colour, bar, out):
+        self.points_number = points_number
+        self.points_colour = points_colour
+        self.bar = bar
+        self.out = out
+
+    def opponent(self, player):
+        return (player + 1) % 2
+
+    def number_of_pieces(board, point):
         return board.points_number[point]
+
+    def colour(board, point):
+        return board.points_colour[point]
+
+    def game_over(board):
+        return ((board.out[0] == 15) or (board.out[1] == 15))
+
+    def has_checker(board, player, fromPoint):
+        return (board.points_colour[fromPoint] == player)
+
+    def has_checkers_on_bar(board, player):
+        return (board.bar[player] != 0)
+
+    def move(board, player, fromPoint, toPoint):  # assume that the labels of points run from 0 to 23
+        """
+        Assuming that the move is legitimate, actually carry it out.
+        The board gets modified!
+        """
+        if board.points_number[fromPoint] == 1:  ## I later realised we had a function for colour...
+            board.points_colour[fromPoint] = 'NO_COLOUR'
+        if board.points_number[toPoint] <= 1:
+            board.points_colour[toPoint] = player
+        board.points_number[fromPoint] = board.points_number[fromPoint] - 1
+        board.points_number[toPoint] = board.points_number[toPoint] + 1
+
     
     
